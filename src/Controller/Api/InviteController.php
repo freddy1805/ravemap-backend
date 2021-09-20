aec77f3ee92650244ce5e6aadf1a4e08703736a9
Frederik Röttgerkamp <frederik.roettgerkamp@q-two.net>
2021-09-20 20:30:30 +0200

    Anpassungen InviteApi

--- src/Controller/Api/InviteController.php
+++ src/Controller/Api/InviteController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\Invite;
 use App\Service\Entity\EventManager;
+use App\Service\Entity\InviteManager;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use OpenApi\Annotations as OA;
 
@@ -20,14 +24,45 @@ class InviteController extends BaseApiController {
     private EventManager $eventManager;
 
     /**
+     * @var InviteManager
+     */
+    private InviteManager $inviteManager;
+
+    /**
      * InviteController constructor.
      * @param ContainerInterface $container
      * @param EventManager $eventManager
+     * @param InviteManager $inviteManager
      */
-    public function __construct(ContainerInterface $container, EventManager $eventManager)
+    public function __construct(ContainerInterface $container, EventManager $eventManager, InviteManager $inviteManager)
     {
         parent::__construct($container);
         $this->eventManager = $eventManager;
+        $this->inviteManager = $inviteManager;
+    }
+
+    /**
+     * @OA\Get(
+     *     operationId="detail",
+     *     summary="Get detailed invite data",
+     *     tags={"Invite"},
+     *     @OA\Response(response="200", description="Returns json object with detailed invite data"),
+     *     @OA\Response(response="401", description="Login faild. Invalid credentials")
+     * )
+     * @Route("/{id}", name="detail", methods={"GET"})
+     */
+    public function detailAction(string $id): Response
+    {
+        /** @var Invite $invite */
+        if ($invite = $this->inviteManager->getById($id)) {
+            if ($invite->getToUser() === null) {
+                return new Response($this->serializeToJson($invite, ['invite_detail']), 200, [
+                    'content-type' => self::JSON_CONTENT_TYPE,
+                ]);
+            }
+        }
+
+        throw new NotFoundHttpException('Invite not found');
     }
 
     /**
